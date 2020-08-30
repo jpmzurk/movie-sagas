31dc5eb467a04fb8c5e61c9057fd6e3a48bd0424
John Patrick Mazurek <jpmzurk@gmail.com>
2020-08-30 11:31:36 -0500

    corrected and updated styling on form, especially lable and error texts. Still needs to be broken into separate components. form component is too big

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -10,30 +10,37 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
 import { useEffect } from 'react';
 import Paper from '@material-ui/core/Paper';
-
-
+import Grid from '@material-ui/core/Grid'
+import Box from '@material-ui/core'
 const useStyles = makeStyles((theme) => ({
     root: {
         '& > *': {
             display: 'flexInline',
             flexDirection: 'column',
+            alignItems: 'center',
+            
         },
     },
     inputs: {
         margin: theme.spacing(1),
-        width: '15ch',
-        // alignItems: 'center',
+        width: '22ch',
+        marginBottom: '1em'
     },
     paper: {
-        display: 'float',
-        alignContent: 'center',
-        margin: '1.1em',
+        margin: 'auto',
         backgroundColor: '#EBEBEB',
-        paddingBottom: '30em',
+        paddingBottom: '16em',
+        marginTop: '2em',
+        width: '60ch',
+    },
+    textField: {
+        margin: theme.spacing(1),
+        width: '40ch',
+        marginBottom: '1em'
     }
 }));
 
-const AddMovie = ({ genres, dispatch }) => {
+const AddMovie = ({ genres, dispatch, history }) => {
     const classes = useStyles();
     const { handleSubmit, reset, register, control } = useForm();
     const [helperText, setHelperText] = useState('');
@@ -43,8 +50,8 @@ const AddMovie = ({ genres, dispatch }) => {
         dispatch({ type: 'FETCH_GENRES' })
     }
 
-    const onSubmit = (data, e) => {
-        if ((data.title === '') || (data.image === '')) {
+    const onSubmit = (data) => {
+        if ((data.title === '') || (data.image === '') || (data.genre_id === '')) {
             setErrorState(true);
             setHelperText('You must enter a title, image, and genre');
         } else {
@@ -55,32 +62,45 @@ const AddMovie = ({ genres, dispatch }) => {
     };
 
     useEffect(getGenres, []);
+
+    const directToMovies = () => {
+        history.push('/')
+    }
+
     return (
-        <Paper className={classes.paper}>
+        <Paper className={classes.paper} >
             <FormControl >
                 <form className={classes.root} onSubmit={handleSubmit(onSubmit)} noValidate autoComplete="off" >
-                    <TextField label="title" name="title" inputRef={register} className={classes.inputs} error={errorState} />
-                    <TextField label="poster image" name="poster" inputRef={register} className={classes.inputs} error={errorState}/>
-                    <InputLabel id="genre label" style={{ marginLeft: "19em" }} className={classes.inputs} >genre</InputLabel>
-                    <Controller control={control} error={errorState} as={<Select labelId="genre" style={{ marginTop: "1.5em", minWidth: '15ch' }}  className={classes.inputs} >
-                        <MenuItem value="no genre selected"></MenuItem>
-                        {genres.map((genre, i) => {
-                            return <MenuItem key={i} value={genre.id}>{genre.name}</MenuItem>
-                        })}
-                    </Select>
-                    } name="genre_id" defaultValue="" />
-                    <section>
-                        <TextField label="description" name="description" inputRef={register} className={classes.inputs} />
-                        <Button type="submit" style={{marginTop:"1.7em"}}> Save </Button>
-                        <Button type="submit" style={{marginTop:"1.7em"}}> CANCEL </Button>
+                    <section style={{ paddingTop: '2em' }}>
+                        <TextField label="title" name="title" inputRef={register} multiline className={classes.inputs} error={errorState} />
                     </section>
-                    <FormHelperText style={{marginLeft: "10em"}}> {helperText} </FormHelperText>
+                    <section>
+                        <TextField label="poster image" name="poster" inputRef={register} multiline className={classes.textField} error={errorState} />
+                    </section>
+                    <section>
+                        <TextField label="description" name="description" inputRef={register} multiline className={classes.textField} />
+                    </section>
+                    <FormControl className={classes.inputs}>
+                   
+                        <InputLabel id="genre" error={errorState}> genre</InputLabel>
+                        <Controller as={<Select labelId="genre" label="genre" className={classes.inputs}>
+                            <MenuItem disabled> genre </MenuItem>
+                            {genres.map((genre, i) => {
+                                return <MenuItem key={i} value={genre.id}>{genre.name}</MenuItem>
+                            })}
+                        </Select>
+                        } name="genre_id" defaultValue="" control={control} error={errorState} />
+                    <FormHelperText  error={errorState} > {helperText} </FormHelperText>
+                    <section className={classes.inputs}> 
+                    <Button type="submit" > Save </Button>
+                    <Button  onClick={directToMovies}> CANCEL </Button>
+                    </section>
+                    </FormControl>
                 </form>
             </FormControl>
         </Paper>
     );
 }
-
 
 const mapPropsToState = (reduxState) => {
     return {
--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-// import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import { connect } from 'react-redux';
